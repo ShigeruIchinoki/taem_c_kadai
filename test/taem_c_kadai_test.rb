4a8fdd7dc0d6a14f3d2e9ca5617eecaeb0032310
ShigeruIchinoki <a1505si@aiit.ac.jp>
2015-08-01 08:41:20 +0000

    メッソッド変更

--- test/taem_c_kadai_test.rb
+++ test/taem_c_kadai_test.rb
@@ -8,9 +8,8 @@ class TaemCKadaiTest < Minitest::Test
   def test_needed_calorie
     assert_equal 761, TaemCKadai.japanese_bee("M", 10)
   end
-
+  #bii
   def test_needed_bmi
     assert_equal 10000, TaemCKadai.needed_bmi(1, 1)
   end
-
 end
