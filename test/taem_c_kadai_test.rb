be275c9af1703256fe794e1aa683f152f61a3646
ShigeruIchinoki <a1505si@aiit.ac.jp>
2015-08-01 08:25:27 +0000

    bmitest add gemspec bugfixwq

--- test/taem_c_kadai_test.rb
+++ test/taem_c_kadai_test.rb
@@ -8,4 +8,9 @@ class TaemCKadaiTest < Minitest::Test
   def test_needed_calorie
     assert_equal 761, TaemCKadai.japanese_bee("M", 10)
   end
+
+  def test_needed_bmi
+    assert_equal 10000, TaemCKadai.japanese_bee(1, 1)
+  end
+
 end
